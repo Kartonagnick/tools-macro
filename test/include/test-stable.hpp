6d79315364588fb395cf31a0e1052c570c2d2754
Kartonagnick <CastleOfDreams@yandex.ru>
2021-02-15 22:08:18 +0300

    add: test

---
+++ test/include/test-stable.hpp
@@ -0,0 +1,43 @@
+
+// [2021y-02m-05d] Idrisov Denis R.
+// [2021y-02m-13d] Idrisov Denis R.
+#pragma once
+
+#ifndef dTEST_STABLE_USED_ 
+#define dTEST_STABLE_USED_ 1
+
+    #define dFORBID_ZERO_SIZE_ARRAY
+    #define INCLUDE_AUTO_GENERATED
+
+    // #define INCLUDE_STRESS_TESTS
+    #define INCLUDE_LONG_LONG_TESTS
+    #define INCLUDE_LONG_TESTS
+
+//==============================================================================
+//===== modern/classic =====================================||==================
+    #define TEST_MODERN                                     // ready!
+    #define TEST_CLASSIC                                    // ready!
+
+//==============================================================================
+//===== tools/macro/args5 ======================================================
+    #define TEST_TOOLS_MACRO_ARGS_5                         // ready!
+    #define TEST_TOOLS_MACRO_COMB_TUPLES_5                  // ready!
+    #define TEST_TOOLS_MACRO_COMB_VALUES_5                  // ready!
+    #define TEST_TOOLS_MACRO_FOREACH_5                      // ready!
+    #define TEST_TOOLS_MACRO_OVERLOAD_5                     // ready!
+    #define TEST_TOOLS_MACRO_REPEAT_5                       // ready!
+
+//==============================================================================
+//===== tools/macro/args7 ======================================================
+    #define TEST_TOOLS_MACRO_ARGS_7                         // ready!
+    #define TEST_TOOLS_MACRO_COMB_TUPLES_7                  // ready!
+    #define TEST_TOOLS_MACRO_COMB_VALUES_7                  // ready!
+    #define TEST_TOOLS_MACRO_FOREACH_7                      // ready!
+    #define TEST_TOOLS_MACRO_OVERLOAD_7                     // ready!
+    #define TEST_TOOLS_MACRO_REPEAT_7                       // ready!
+
+//==============================================================================
+//==============================================================================
+
+#endif // !dTEST_STABLE_USED_
+

62d2be3ed08c259973168857dbb1946b05b4f580
Kartonagnick <CastleOfDreams@yandex.ru>
2021-05-08 23:32:05 +0300

    add: macro.hpp (101) PRE

---
+++ include/tools/macro/macro.hpp
@@ -0,0 +1,22 @@
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+ created: 2021y-04m-20d 16:12:59 April                      +
+//+ changed: 2021y-05m-08d 21:12:59 April                      +
+//+ macro.hpp                                                  +
+//+                                            Tools's library +
+//+                         Copyright @ 2021, Castle Of Dreams +
+//+                                     [author: Idrisov D. R] +
+//+                                   CastleOfDreams@yandex.ru +
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#pragma once
+#ifndef dTOOLS_MACRO_USED_
+#define dTOOLS_MACRO_USED_ 101 PRE
+
+#define dMACRO_MAJOR 1
+#define dMACRO_MINOR 0
+#define dMACRO_PATCH 1
+
+//==============================================================================
+//==============================================================================
+#endif // !dTOOLS_MACRO_USED_
